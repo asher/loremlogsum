0ff4a16ba682d21a1827a6f77caae40b98034a44
Asher Feldman <asher@bandpage.com>
2014-08-02 08:44:55 -0700

    shape rate via --burst and --variance

--- README.md
+++ README.md
@@ -1,9 +1,58 @@
 loremlogsum
 ===========
-Generate mock logdata to stdout of a configurable line length, for a configurable time.
+Generate mock logdata to stdout or via http get, for a configurable time.
+Defaults to sending at a steady per-second rate. If --spike is set, rate
+becomes an approximate floor (randomized per second +/- a factor of --variance)
+with a 1:variance chance of bursting to --rate * 10 in any given second.
 
 ## Usage
 ```
 loremlogsum.py --time=5 --length=200 > testlog
 ```
 Generates 200 character log lines for 5 seconds to the file testlog.
+
+```
+$ ./loremlogsum.py --help
+usage: loremlogsum.py [-h] [--time seconds] [--length characters]
+                      [--rate lines/sec attempted] [--module writer]
+                      [--endpoint http://logs.to/here] [--key api_key]
+                      [--burst] [--variance 0.2]
+
+Generate test log data
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --time seconds        time to run in seconds (default 5)
+  --length characters   max number of characters per log line (default 120)
+  --rate lines/sec attempted
+                        max lines/sec to attempt writing (default 8192)
+  --module writer       set log sending module [stdout, http] (default stdout
+  --endpoint http://logs.to/here
+                        where to send logs if module != stdout
+  --key api_key         api key, required for network modules
+  --burst               randomize base rate around variance, with random
+                        bursts to 10 * rate
+  --variance 0.2        variance when using --burst, where base log rate will
+                        randomly vary between rate * (1-variance) and rate *
+                        (1+variance)
+```
+
+## Example
+```
+$ time python loremlogsum.py --time=10 --length=100 --rate=500 --burst --variance=0.26 > out
+real    0m10.121s
+user    0m1.008s
+sys 0m0.036s
+
+$ awk '{print $1}' out | uniq -c
+ 413 1406994187
+ 417 1406994188
+ 556 1406994189
+5000 1406994190
+ 516 1406994191
+ 545 1406994192
+ 421 1406994193
+ 612 1406994194
+ 545 1406994195
+5000 1406994196
+```
--- loremlogsum.py
+++ loremlogsum.py
@@ -13,23 +13,28 @@ def make_logs(timestamp, size, chars=string.ascii_lowercase + string.ascii_upper
 
 def main():
     parser = argparse.ArgumentParser(description="Generate test log data")
-    parser.add_argument("--time", metavar="seconds", required=False, default=20,
-                        type=int, help="time to run in seconds (default 20)")
+    parser.add_argument("--time", metavar="seconds", required=False, default=5,
+                        type=int, help="time to run in seconds (default 5)")
     parser.add_argument("--length", metavar="characters", default=120,
                         type=int, help="max number of characters per log line (default 120)")
     parser.add_argument("--rate", metavar="lines/sec attempted", default=8192,
-                        type=int, help="max lines/sec to attempt writing (default 120)")
+                        type=int, help="max lines/sec to attempt writing (default 8192)")
     parser.add_argument("--module", metavar="writer", default='stdout',
-                        type=str, help="set log sending module i.e. stdout, http")
+                        type=str, help="set log sending module [stdout, http] (default stdout")
     parser.add_argument("--endpoint", metavar="http://logs.to/here", type=str,
                         help="where to send logs if module != stdout")
     parser.add_argument("--key", metavar="api_key", type=str,
-                        help="api key, required for network (http, etc) mode")
-    parser.add_argument("--spike", default=False, action='store_true',
-                        help="steady at rate, with bursts")
+                        help="api key, required for network modules")
+    parser.add_argument("--burst", default=False, action='store_true',
+                        help="randomize base rate around variance, with random bursts to\
+                        10 * rate")
+    parser.add_argument("--variance", metavar="0.2", default=0.2,
+                        type=float, help="variance when using --burst, where base log rate\
+                        will randomly vary between rate * (1-variance) and\
+                        rate * (1+variance)")
     args = parser.parse_args()
 
-    variance = 0.2 # TODO: make a param
+    variance = args.variance
 
     if args.module == 'http':
         logger = loremlogsum_http.loremlogsum_http(args.key, args.endpoint)
@@ -38,20 +43,21 @@ def main():
 
     start = int(time.time())
     now = int(time.time())
+    length = args.length - len(str(now)) - 1
 
     while start + args.time > now:
         throttle = time.time()
 
-        if args.spike:
+        if args.burst:
             if random.choice(range(0, 100)) < 100 * variance:
                 rate = args.rate * 10
             else:
                 rate = random.choice(range(int(args.rate * (1 - variance)), int(args.rate * (1 + variance))))
-
         else:
             rate = args.rate
+
         for i in range(0,rate):
-            logger.send(make_logs(str(now), args.length))
+            logger.send(make_logs(str(now), length))
         if time.time() < throttle + 1:
             time.sleep(throttle + 1 - time.time())
         now = int(time.time())
